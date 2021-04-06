cc55fe2bb3213623d2dc02380749856f16730cdd
Raytwo <Raytwost@gmail.com>
2021-04-06 02:14:03 +0200

    Add debug logging to API methods

--- src/api.rs
+++ src/api.rs
@@ -1,12 +1,18 @@
+use log::debug;
 use smash_arc::Hash40;
 
 #[no_mangle]
 pub extern "C" fn arcrop_load_file(hash: Hash40, out_buffer: *mut u8, length: usize) {
+    debug!("[Arcropolis-API::load_file] Hash40: {:#x}, Buffer len: {:#x}", hash.as_u64(), length);
     let buffer = unsafe { std::slice::from_raw_parts_mut(out_buffer, length) };
     
     unimplemented!()
 }
 
 pub extern "C" fn arcrop_register_callback(hash: Hash40) {
-    unimplemented!()
+    debug!("[Arcropolis-API::register_callback] hash: {:#x}", hash.as_u64());
+}
+
+pub extern "C" fn arcrop_api_version() {
+    debug!("[Arcropolis-API::api_version] Function called");
 }
