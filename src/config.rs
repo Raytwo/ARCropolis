ef81f5a9107cab28e2cf35199e8b28136aa6a529
blu-dev <the.blu.dev@gmail.com>
2021-09-15 19:32:47 -0700

    Config and logger (#128)
    * rewrite config
    
    * further develop logger

---
+++ src/config.rs
@@ -0,0 +1,194 @@
+use serde_derive::{Deserialize, Serialize};
+
+static CONFIG_PATH: &'static str = "sd:/ultimate/arcropolis/config.toml";
+
+lazy_static! {
+    static ref GLOBAL_CONFIG: Config = {
+        let config = match std::fs::read_to_string(CONFIG_PATH) {
+            Ok(toml) => {
+                match toml::de::from_str(toml.as_str()) {
+                    Ok(config) => Config::from_intermediate(config),
+                    Err(_) => {
+                        warn!("Unable to read config file, generating new one.");
+                        Config::new()
+                    }
+                }
+            },
+            Err(_) => {
+                warn!("Unable to read config file, generating new one.");
+                Config::new()
+            }
+        };
+
+        match toml::ser::to_string_pretty(&config) {
+            Ok(string) => {
+                match std::fs::write(CONFIG_PATH, string.as_bytes()) {
+                    Err(_) => warn!("Unable to write config file."),
+                    _ => {}
+                }
+            },
+            Err(_) => warn!("Failed to serialize config data.")
+        }
+
+        config
+    };
+}
+
+trait FromIntermediate<I> {
+    fn from_intermediate(int: I) -> Self;
+}
+
+#[derive(Serialize)]
+struct Config {
+    pub version: String,
+    pub debug: bool,
+    pub auto_update: bool,
+    pub paths: ConfigPaths,
+    pub logger: ConfigLogger
+}
+
+impl Config {
+    pub fn new() -> Self {
+        Self {
+            version: String::from(env!("CARGO_PKG_VERSION")),
+            debug: false,
+            auto_update: true,
+            paths: ConfigPaths::new(),
+            logger: ConfigLogger::new()
+        }
+    }
+}
+
+impl FromIntermediate<IConfig> for Config {
+    fn from_intermediate(int: IConfig) -> Self {
+        let version = String::from(env!("CARGO_PKG_VERSION")); // We don't care what the previous version was
+        let IConfig { debug, auto_update, paths, logger, ..} = int;
+        let debug = debug.unwrap_or(false);
+        let auto_update = auto_update.unwrap_or(true);
+        let paths = paths
+            .map_or(ConfigPaths::new(), |x| ConfigPaths::from_intermediate(x));
+        let logger = logger
+            .map_or(ConfigLogger::new(), |x| ConfigLogger::from_intermediate(x));
+        Self {
+            version,
+            debug,
+            auto_update,
+            paths,
+            logger
+        }
+    }
+}
+
+#[derive(Deserialize)]
+struct IConfig {
+    pub version: Option<String>,
+    pub debug: Option<bool>,
+    pub auto_update: Option<bool>,
+    pub paths: Option<IConfigPaths>,
+    pub logger: Option<IConfigLogger>
+}
+
+#[derive(Serialize)]
+struct ConfigPaths {
+    pub arc: String,
+    pub umm: String,
+    pub extra_paths: Vec<String>
+}
+
+impl ConfigPaths {
+    fn new() -> Self {
+        Self {
+            arc: String::from("rom:/arc"),
+            umm: String::from("sd:/ultimate/mods"),
+            extra_paths: Vec::new()
+        }
+    }
+}
+
+impl FromIntermediate<IConfigPaths> for ConfigPaths {
+    fn from_intermediate(int: IConfigPaths) -> Self {
+        let IConfigPaths { arc, umm, extra_paths } = int;
+        let arc = arc.unwrap_or(String::from("rom:/arc"));
+        let umm = umm.unwrap_or(String::from("sd:/ultimate/mods"));
+        let extra_paths = extra_paths.unwrap_or(Vec::new());
+        Self {
+            arc,
+            umm,
+            extra_paths
+        }
+    }
+}
+
+#[derive(Deserialize)]
+struct IConfigPaths {
+    pub arc: Option<String>,
+    pub umm: Option<String>,
+    pub extra_paths: Option<Vec<String>>
+}
+
+#[derive(Serialize)]
+struct ConfigLogger {
+    pub logger_level: String,
+    pub log_to_file: bool
+}
+
+impl ConfigLogger {
+    pub fn new() -> Self {
+        Self {
+            logger_level: String::from("Warn"),
+            log_to_file: false
+        }
+    }
+}
+
+impl FromIntermediate<IConfigLogger> for ConfigLogger {
+    fn from_intermediate(int: IConfigLogger) -> Self {
+        let IConfigLogger { logger_level, log_to_file } = int;
+
+        let logger_level = logger_level.unwrap_or(String::from("Warn"));
+        let log_to_file = log_to_file.unwrap_or(false);
+
+        Self {
+            logger_level,
+            log_to_file
+        }
+    }
+}
+
+#[derive(Deserialize, Serialize)]
+struct IConfigLogger {
+    pub logger_level: Option<String>,
+    pub log_to_file: Option<bool>
+}
+
+pub fn auto_update_enabled() -> bool {
+    GLOBAL_CONFIG.auto_update
+}
+
+pub fn debug_enabled() -> bool {
+    GLOBAL_CONFIG.debug
+}
+
+pub fn version() -> &'static str {
+    GLOBAL_CONFIG.version.as_str()
+}
+
+pub fn arc_path() -> &'static str {
+    GLOBAL_CONFIG.paths.arc.as_str()
+}
+
+pub fn umm_path() -> &'static str {
+    GLOBAL_CONFIG.paths.umm.as_str()
+}
+
+pub fn extra_paths() -> &'static Vec<String> {
+    &GLOBAL_CONFIG.paths.extra_paths
+}
+
+pub fn logger_level() -> &'static str {
+    GLOBAL_CONFIG.logger.logger_level.as_str()
+}
+
+pub fn file_logging_enabled() -> bool {
+    GLOBAL_CONFIG.logger.log_to_file
+}
--- src/logging.rs
+++ src/logging.rs
@@ -1,4 +1,95 @@
+use std::{fs::File, io::{BufWriter, Write}, ops::Deref, path::PathBuf, time::SystemTime};
 use log::{LevelFilter, Metadata, Record, SetLoggerError};
+use parking_lot::Mutex;
+
+use crate::config;
+use regex::Regex;
+
+fn format_time_string(seconds: u64) -> String {
+    let leapyear = |year| -> bool {
+        year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
+    };
+
+    static YEAR_TABLE: [[u64; 12]; 2] = [
+        [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ],
+        [ 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ]
+    ];
+
+    let mut year = 1970;
+
+    let seconds_in_day = seconds % 86400;
+    let mut day_number = seconds / 86400;
+
+    let sec = seconds_in_day % 60;
+    let min = (seconds_in_day % 3600) / 60;
+    let hours = seconds_in_day / 3600;
+    loop {
+        let year_length = if leapyear(year) {
+            366
+        } else {
+            365
+        };
+
+        if day_number >= year_length {
+            day_number -= year_length;
+            year += 1;
+        } else {
+            break;
+        }
+    }
+    let mut month = 0;
+    while day_number >= YEAR_TABLE[if leapyear(year) { 1 } else { 0 }][month] {
+        day_number -= YEAR_TABLE[if leapyear(year) { 1 } else { 0 }][month];
+        month += 1;
+    }
+    format!("{:04}-{:02}-{:02}_{:02}-{:02}-{:02}", year, month + 1, day_number + 1, hours, min, sec)
+}
+
+fn strip_color<S: AsRef<str>>(string: S) -> String {
+    let re = Regex::new("\x1b\\[.[^m]*.").unwrap();
+    re.replace_all(string.as_ref(), "").to_string()
+}
+
+static LOG_PATH: &'static str = "sd:/ultimate/arcropolis/logs";
+static FILE_LOG_BUFFER: usize = 0x2000;
+struct FileLogger(Option<Mutex<BufWriter<File>>>);
+
+impl Deref for FileLogger {
+    type Target = Option<Mutex<BufWriter<File>>>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl FileLogger {
+    pub fn write(&self, message: String) {
+        if let Some(writer) = &self.0 {
+            let mut writer = writer.lock();
+            let _ = writer.write(message.as_bytes());
+        }
+    }
+}
+
+lazy_static! {
+    static ref FILE_WRITER: FileLogger = {
+        let seconds = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .expect("Clock may have gone backwards!");
+        let path = PathBuf::from(LOG_PATH).join(format!(
+            "{}.log",
+            format_time_string(seconds.as_secs())
+        ));
+        std::fs::File::create(path)
+            .map_or_else(
+                |_| {
+                    error!(target: "std", "Unable to initialize the file logger!");
+                    FileLogger(None)
+                },
+                |file| FileLogger(Some(Mutex::new(BufWriter::with_capacity(FILE_LOG_BUFFER, file))))
+            )
+    };
+}
 
 struct ArcLogger;
 
@@ -14,10 +105,55 @@ impl log::Log for ArcLogger {
     }
 
     fn log(&self, record: &Record) {
-        if self.enabled(record.metadata()) {
-            println!("{}", record.args());
+        if !self.enabled(record.metadata()) {
+            return;
+        }
+
+        let module_path = match record.module_path() {
+            Some(path) => path,
+            None => return
+        };
+
+        let message = if record.level() == LevelFilter::Debug {
+            let file = match record.file() {
+                Some(file) => file,
+                None => "???"
+            };
+            let number = match record.line() {
+                Some(no) => format!("{}", no),
+                None => "???".to_string()
+            };
+            format!("[{} | {}:{}] {}\n", module_path, file, number, record.args())
+        } else {
+            format!("[{}] {}\n", module_path, record.args())
+        };
+
+        match record.target() {
+            "std" => {
+                print!("{}", message);
+            },
+            "file" => {
+                if config::file_logging_enabled() {
+                    FILE_WRITER.write(strip_color(message));
+                }
+            },
+            _ => {
+                print!("{}", message);
+                if config::file_logging_enabled() {
+                    FILE_WRITER.write(strip_color(message));
+                }
+            }
         }
     }
 
-    fn flush(&self) {}
+    fn flush(&self) {
+        if config::file_logging_enabled() {
+            if let Some(writer) = &**FILE_WRITER {
+                let mut writer = writer.lock();
+                if let Err(err) = writer.flush() {
+                    error!(target: "std", "Failed to flush file logger! Reason: {:?}", err)
+                }
+            }
+        }
+    }
 }
