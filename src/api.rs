ba99e3641723b325a72820fd085821eda441d2e0
blu-dev <the.blu.dev@gmail.com>
2022-01-26 20:57:14 -0700

    version bump + fix arcrop_require_api_version

--- src/api.rs
+++ src/api.rs
@@ -33,3 +33,32 @@ pub extern "C" fn arcrop_api_version() -> &'static ApiVersion {
 
     &API_VERSION
 }
+
+pub fn show_dialog(text: &str) {
+    skyline_web::DialogOk::ok(text);
+}
+
+#[no_mangle]
+pub extern "C" fn arcrop_require_api_version(major: u32, minor: u32) {
+    if major > API_VERSION.major || (major == API_VERSION.major && minor > API_VERSION.minor) {
+        show_arcrop_update_prompt()
+    } else if major < API_VERSION.major {
+        show_plugin_update_prompt()
+    }
+}
+
+fn show_arcrop_update_prompt() -> ! {
+    show_dialog(
+        "Your ARCropolis version is older than one of your plugins supports, an update is required",
+    );
+
+    unsafe { skyline::nn::oe::ExitApplication() }
+}
+
+fn show_plugin_update_prompt() -> ! {
+    show_dialog(
+        "Your ARCropolis version is too new for one of your plugins, it must be updated to support this API version"
+    );
+
+    unsafe { skyline::nn::oe::ExitApplication() }
+}
