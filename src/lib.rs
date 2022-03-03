34bba0b5b95a9c3a22a8bfb3fb8cafec72119bae
Raytwo <raytwost@gmail.com>
2022-03-04 00:55:56 +0100

    Update prompt will now show the No button as the default

--- src/lib.rs
+++ src/lib.rs
@@ -332,10 +332,14 @@ pub fn main() {
             .spawn(|| {
                 if config::auto_update_enabled() {
                     update::check_for_updates(config::beta_updates(), |update_kind| {
-                        skyline_web::Dialog::yes_no(format!(
-                            "{} has been detected. Do you want to install it?",
-                            update_kind
-                        ))
+                        // skyline_web::Dialog::yes_no(format!(
+                        //     "{} has been detected. Do you want to install it?",
+                        //     update_kind
+                        // ))
+                        match skyline_web::Dialog::new(format!("{} has been detected. Do you want to install it?", update_kind), "No", "Yes").show() {
+                            skyline_web::DialogOption::Left => false,
+                            skyline_web::DialogOption::Right => true,
+                        }
                     });
                 }
             })
