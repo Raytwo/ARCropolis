9cfdef4cfe06243a936efa6ce04e2504d9f9a0fd
Raytwo <raytwost@gmail.com>
2022-03-04 01:34:14 +0100

    Remove duplicate show_error

--- src/lib.rs
+++ src/lib.rs
@@ -366,7 +366,7 @@ pub fn main() {
         };
 
         let err_msg = format!("thread has panicked at '{}', {}", msg, location);
-        show_error(
+        skyline::error::show_error(
             69,
             "Skyline plugin as panicked! Please open the details and send a screenshot to the developer, then close the game.\n",
             err_msg.as_str()
@@ -390,32 +390,4 @@ pub fn main() {
     let _ = resources.join();
 
     api::event::setup();
-}
-
-fn show_error(code: u32, message: &str, details: &str) {
-    use skyline::nn::{err, settings};
-
-    let mut message_bytes = String::from(message).into_bytes();
-    let mut details_bytes = String::from(details).into_bytes();
-
-    if message_bytes.len() >= 2048 {
-        message_bytes.truncate(2044);
-        message_bytes.append(&mut String::from("...\0").into_bytes());
-    }
-
-    if details_bytes.len() >= 2048 {
-        details_bytes.truncate(2044);
-        details_bytes.append(&mut String::from("...\0").into_bytes());
-    }
-
-    unsafe {
-        let error = err::ApplicationErrorArg::new_with_messages(
-            code,
-            core::str::from_utf8(&message_bytes).unwrap().as_bytes().as_ptr(),
-            core::str::from_utf8(&details_bytes).unwrap().as_bytes().as_ptr(),
-            &settings::LanguageCode_Make(settings::Language_Language_English)
-        );
-
-        err::ShowApplicationError(&error);
-    }
 }
