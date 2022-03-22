bfa3e674c6d1bd687e4d15ba43a821677fe68ce8
blu-dev <the.blu.dev@gmail.com>
2022-03-21 22:48:58 -0700

    fix unsharing/replacing prebuilt files

--- src/fs.rs
+++ src/fs.rs
@@ -451,8 +451,9 @@ impl CachedFilesystem {
         });
 
         /// Don't unshare any files in the unshare blacklist (nus3audio handled during filesystem finish)
-        let files = self.hash_lookup.iter().filter_map(|(hash, _)| {
+        let files = self.hash_lookup.iter().filter_map(|(hash, path)| {
             if self.config.unshare_blacklist.contains(&Hash40String(*hash)) {
+                println!("blacklisting file: {:#x}", hash.0);
                 None
             } else {
                 Some(*hash)
