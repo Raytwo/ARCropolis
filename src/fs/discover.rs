d0fb53bf2ff8f275fcfc7bcd0668fdc5ad8144a6
Raytwo <raytwost@gmail.com>
2022-04-20 20:58:34 +0200

    Ditto but for real this time

--- src/fs/discover.rs
+++ src/fs/discover.rs
@@ -42,14 +42,16 @@ pub fn perform_discovery() -> LaunchPad<StandardLoader> {
 
     if is_emulator {
         info!("Emulator usage detected in perform_discovery, reverting to old behavior.");
+    }
 
+    let legacy_discovery = config::legacy_discovery();
+
+    if !is_emulator {
         // Open the ARCropolis menu if Minus is held before mod discovery
         if ninput::any::is_down(ninput::Buttons::PLUS) {
             crate::menus::show_main_menu();
         }
     }
-
-    let legacy_discovery = config::legacy_discovery();
 
     let filter = |path: &Path| {
         // If we're not running on emulator
