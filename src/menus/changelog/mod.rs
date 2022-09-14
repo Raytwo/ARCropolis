1e9ec16f62096b783cab0e3620a4909c7f3276aa
Ali Hussain <alihussain2001@gmail.com>
2022-09-13 20:00:24 -0400

    Remove ramhorn dependancy from Changelog (#424)
    Also add jozz and Coolsonickirby to Maintainers

--- src/menus/changelog/mod.rs
+++ src/menus/changelog/mod.rs
@@ -1,12 +1,12 @@
-use skyline_web::{ramhorns, Webpage};
+use skyline_web::Webpage;
 
-#[derive(ramhorns::Content, serde::Deserialize)]
+#[derive(serde::Deserialize)]
 pub struct NotesEntry {
     pub section_title: String,
     pub contents: String,
 }
 
-#[derive(ramhorns::Content, serde::Deserialize)]
+#[derive(serde::Deserialize)]
 pub struct MainEntry {
     pub title: String,
     pub date: String,
@@ -14,14 +14,34 @@ pub struct MainEntry {
     pub entries: Vec<NotesEntry>,
 }
 
+pub fn build_html(info: &MainEntry) -> String {
+    let mut rendered = crate::menus::files::CHANGELOG_HTML_TEXT.to_string();
+    rendered = rendered.replace("{{title}}", &info.title);
+    rendered = rendered.replace("{{date}}", &info.date);
+    rendered = rendered.replace("{{description}}", &info.description);
+    
+    let entries = info.entries.iter().map(|entry| {
+        format!("
+        <div class=\"section\">
+            <h2 class=\"section-header\">
+                <div>{}</div>
+            </h2>
+            <div>
+                {}
+            </div>
+        </div>
+        ", entry.section_title, entry.contents)
+    }).collect::<Vec<String>>().join("\n");
+    
+    rendered = rendered.replace("{{entries}}", &entries);
+
+    rendered
+}
+
 pub fn display_update_page(info: &MainEntry) {
-    let tpl = ramhorns::Template::new(crate::menus::files::CHANGELOG_HTML_TEXT).unwrap();
-
-    let render = tpl.render(&info);
-
     Webpage::new()
         .htdocs_dir("contents")
-        .file("index.html", &render)
+        .file("index.html", &build_html(info))
         .file("notes.png", &crate::menus::files::CHANGELOG_IMAGE_BYTES)
         .background(skyline_web::Background::Default)
         .boot_display(skyline_web::BootDisplay::Default)
