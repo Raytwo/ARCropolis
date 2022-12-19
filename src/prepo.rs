4e4b84f189335a92c9ced6feff7520a941b0af95
jozz024 <74272560+jozz024@users.noreply.github.com>
2022-12-19 13:42:18 -0600

    add immediate transmission hook

--- src/prepo.rs
+++ src/prepo.rs
@@ -1,5 +1,5 @@
 use super::logging;
-use std::time::SystemTime;
+use std::{time::SystemTime, path::PathBuf, fs::DirEntry};
 use skyline::nn;
 
 #[repr(C)]
@@ -17,7 +17,7 @@ fn prepo_save(prepo: &PlayReport, uid: &nn::account::Uid) {
     let event_id = unsafe { skyline::from_c_str(prepo.event_id.as_ptr()) };
     println!("Event id: {}", event_id);
     skyline::logging::hex_dump_ptr(prepo.buffer);
-    unsafe { 
+    unsafe {
         let mut buffer = std::io::Cursor::new(std::slice::from_raw_parts(prepo.buffer, prepo.position));
         let test = rmpv::decode::read_value(&mut buffer).unwrap();
         let json_data = serde_json::to_string_pretty(&test).unwrap();
@@ -29,13 +29,26 @@ fn prepo_save(prepo: &PlayReport, uid: &nn::account::Uid) {
 
         let file_path = format!("sd:/ultimate/arcropolis/prepo/{}-{}.json", event_id, logging::format_time_string(seconds.as_secs()));
 
-        if !event_id.starts_with("sum_") {
-            std::fs::write(file_path, json_data);
-        }
+        std::fs::write(file_path, json_data).unwrap();
     }
-    call_original!(dbg!(prepo), uid);
+    if !event_id.starts_with("arc_") {
+        call_original!(dbg!(prepo), uid);
+    }
 }
 
+#[skyline::hook(offset = 0x39c4a60)]
+fn immediate_transmission() {
+    let prepo_reader = std::fs::read_dir("sd:/ultimate/arcropolis/prepo").expect("Dir missing?");
+    let mut prepo_logs: Vec<String> = prepo_reader.into_iter().map(|p| p.unwrap().path().display().to_string()).collect();
+    send_logs_to_api(prepo_logs);
+    call_original!()
+}
+
+fn send_logs_to_api(prepo_logs: Vec<String>) {
+    unimplemented!()
+}
+
+
 pub fn install() {
-    skyline::install_hook!(prepo_save);
+    skyline::install_hooks!(prepo_save, immediate_transmission);
 }
